a0d4335a79308774d491dde67c9b0f4c39a36d04
kokolegorille <koko.le.gorille@gmail.com>
2020-05-02 23:26:33 +0200

    Show sample components

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import signinSchema from '../schemas/signin_schema';
+
+import Form from '../components/form';
 import MenuIcon from '../components/menu_icon';
 import Joystick from '../components/joystick';
 
@@ -7,8 +10,20 @@ const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <h2>Sample Form</h2>
+      <Form 
+        schema={signinSchema} 
+        callback={params => console.log(params)} 
+        handleCancel={() => console.log("Cancel")} 
+        resetOnSubmit={true} />
+      <h2>Sample Menu Icon</h2>
       <MenuIcon callback={() => console.log("Clicked!")} />
-      <Joystick callback={() => console.log("Moved!")} />
+      <h2>Sample Joystick</h2>
+      <Joystick 
+        callback={
+          ({forward, turn}) => console.log(`Moved: ${forward} ${turn}`)
+        } 
+      />
     </>
   );
 };
